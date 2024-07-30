877248266e5aca65d5dfcd63d556d45a35aba7ce
Priti Gaikwad <pritisgaikwad25@gmail.com>
2024-07-30 16:04:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] A = [-1, -3];
+
+            //int[] A = [1, 2, 3];
+
+            int[] A = [5, 3, -6, -4, 10, 2];
+
+            Solution solution = new Solution();
+
+            int min=solution.solution(A);
+
+            Console.WriteLine("Square of Minimum Integer is: "+ (min*min));
+
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    internal class Solution
+    {
+      
+        public int solution(int[] A)
+        {
+
+            int min = int.MaxValue;
+
+            bool positive = false;
+
+            foreach (int i in A)
+            {
+               if(i>0)
+                {
+                    min = Math.Min(min, i);
+                    positive = true; 
+                       
+                }  
+            }
+         return  positive ? min : 0; 
+
+            
+           
+        }
+    }
+}
